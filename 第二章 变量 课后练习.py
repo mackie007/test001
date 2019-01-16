bec57a02a641b9d6e5adf8864ee1eb7f2bc66b29
mackie007 <784350547@qq.com>
2019-01-16 16:16:57 +0800

    Initial commit

---
+++ 第二章 变量 课后练习.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python 
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+name_message = 'Albert Einstein once said, “A person who never made a mistake never tried anything new.”'
+print(name_message.upper())
+print(name_message.lower())
+print(name_message.title())
+
+famous_person = 'Albert Einstein'
+message = famous_person + 'once said, “A person who never made a mistake never tried anything new.”'
+print(message)
+
+name = " Alb\tert Ei\nnstein "
+print(name)
+print(name.rstrip())
+print(name.lstrip())
+print(name.strip())
