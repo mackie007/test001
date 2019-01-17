8a30d0df157cffa01b213b278a3741a76147bd82
mackie007 <784350547@qq.com>
2019-01-17 10:48:39 +0800

    第三章

--- 第三章 变量 课后练习.py
+++ 第三章 变量 课后练习.py
@@ -57,6 +57,57 @@ for d in range(0,len(list)-2):
     print('很抱歉不能与你共进晚餐了' + popped_list)
 for e in range(0,len(list)):
     print('我依然可以邀请'+ list[e] + '吃法国鹅肝')
+del list[1]#del 因从后往前删，不然就会报错
 del list[0]
-del list[1]
+print(list)
+
+# 3-8 放眼世界 ： 想出至少5个你渴望去旅游的地方。
+# 将这些地方存储在一个列表中， 并确保其中的元素不是按字母顺序排列的。
+# 按原始排列顺序打印该列表。 不要考虑输出是否整洁的问题， 只管打印原始Python列表。
+# 使用sorted( ) 按字母顺序打印这个列表， 同时不要修改它。
+# 再次打印该列表， 核实排列顺序未变。
+# 使用sorted( ) 按与字母顺序相反的顺序打印这个列表， 同时不要修改它。
+# 再次打印该列表， 核实排列顺序未变。
+# 使用reverse( ) 修改列表元素的排列顺序。 打印该列表， 核实排列顺序确实变了。
+# 使用reverse( ) 再次修改列表元素的排列顺序。 打印该列表， 核实已恢复到原来的排列顺序。
+# 使用sort( ) 修改该列表， 使其元素按字母顺序排列。 打印该列表， 核实排列顺序确实变了。
+# 使用sort( ) 修改该列表， 使其元素按与字母顺序相反的顺序排列。 打印该列表， 核实排列顺序确实变了。
+# 3-9 晚餐嘉宾 ： 在完成练习 3-4~练习 3-7时编写的程序之一中， 使用len( ) 打印一条消息， 指出你邀请了多少位嘉宾来与你共进晚餐。
+# 3-10 尝试使用各个函数 ： 想想可存储到列表中的东西， 如山岳、 河流、 国家、 城市、 语言或你喜欢的任何东西。 编写一个程序， 在其中创建一个包含这些元素的列
+# 表， 然后， 对于本章介绍的每个函数， 都至少使用一次来处理这个列表。
+place = ['taiguo','meiguo','xinjiapo']
+print(place)
+print(sorted(place))
+print(place)
+print(sorted(place,reverse=True))
+print(place)
+place.reverse()
+print(place)
+place.reverse()
+print(place)
+place.sort(reverse=True)
+print(place)
+
+list = ['成龙','李连杰','郭富城']
+for b in range(0,3):
+    print('我想和' + list[b] + '一起吃晚饭。')
+print(list[2] + '无法赴约')
+list.insert(2,'洪金宝')
+print('我想和' + list[2] + '一起吃晚饭。')
+print('\n哈哈，皇天不负有心人，找到更大的餐桌。')
+list.insert(0,'王宝强')
+list.insert(2,'马蓉')
+list.append('王菊')
+for c in range(0,6):
+    print('邀请' + list[c] + "一起吃饭")
+print('\n送餐桌的快递车着火了,所以现在只能邀请两位来了\n')
+print(list)
+for d in range(0,len(list)-2):
+    popped_list = list.pop()
+    print('很抱歉不能与你共进晚餐了' + popped_list)
+for e in range(0,len(list)):
+    print('我依然可以邀请'+ list[e] + '吃法国鹅肝')
+    print('我将邀请' + str(len(list))+'个人吃大餐')
+del list[1]#del 因从后往前删，不然就会报错
+del list[0]
 print(list)
